6bb1a8e83ae7dbf40cd8d9a84c6906601dd212c7
wendy hsiao <wendy19940331@gmail.com>
2019-10-15 23:00:39 +0800

    Add route

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const express = require('express')
 const app = express()
+port = 3000
+
+app.get('/', (req, res) => {
+  res.send('首頁')
+})
+
+app.post('/', (req, res) => {
+  res.send('送出短網址')
+})
+
+app.listen(port, () => {
+  console.log('App is running')
+})
